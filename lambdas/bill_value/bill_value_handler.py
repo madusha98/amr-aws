f10aa241b368918d249327e02063f6f694995bd8
Praveen Geevinda <praveen.2018836@iit.ac.lk>
2021-03-12 23:30:56 +0530

    modified the python function to accept JSON reuqest body

--- lambdas/bill_value/bill_value_handler.py
+++ lambdas/bill_value/bill_value_handler.py
@@ -1,23 +1,36 @@
 import requests
 import json
 
+
 def get_bill_value(event, context):
 
+    # 1. partse out query string parameters
+    request = json.loads(event['body'])
+    noOfUnits = request['NoOfUnits']
+    fromDate = request['FromDate']
+    toDate = request['ToDate']
+
+    # 2. construct the header and body of the CEB endpoint
     headers = {
         'X-Requested-With': 'XMLHttpRequest',
     }
 
     data = {'tariff_cat': '11',
-            'iUnit': '150',
-            'datepicker3': '2021-03-01',
-            'datepicker4': '2021-03-31',
+            'iUnit': noOfUnits,
+            'datepicker3': fromDate,
+            'datepicker4': toDate,
             'lastsegment': 'en'}
-
+    
+    # 3. construct the http response from CEB endpoint
     response = requests.post(
         "https://ceb.lk/bill_calculation_commercial/calculate_bill", headers=headers, data=data, verify=False)
 
-    print(response.text)
-    response_load = json.loads(response.text)
-    print('Bill Value:', response_load['Total'])
+    # response_load = json.loads(response.text)
+    # print('Bill Value:', response_load['Total'])
 
-    return response
+    # 4. return the repsonse object
+    response = json.loads(response.text)
+    return {
+        "body": json.dumps(response)
+    }
+
