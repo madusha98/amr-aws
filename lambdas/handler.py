6131e25ba4b30c511730b25dc9215cf0caa111ed
Sudeeptha Rahal Goonewardene <42436258+rahalgoon@users.noreply.github.com>
2021-03-30 23:34:22 +0530

    Commenting test codes

--- lambdas/handler.py
+++ lambdas/handler.py
@@ -6,14 +6,14 @@ def hello(event, context):
     body = {
         "message": "Go Serverless v1.0! Your function executed successfully!"
     }
-    dynamodb = boto3.resource('dynamodb')
-    try:
-        table = dynamodb.Table('billTable')
-        item = table.get_item(Key={'accId': 'abc'})
-        print(item)
+    # dynamodb = boto3.resource('dynamodb')
+    # try:
+    #     table = dynamodb.Table('billTable')
+    #     item = table.get_item(Key={'accId': 'abc'})
+    #     print(item)
 
-    except Exception as e:
-        print('Exception: ', e)
+    # except Exception as e:
+    #     print('Exception: ', e)
 
 
     response = {
