a6bd885b4a21c11d222324062146884243d2f479
madushalakruwan <madusha.2018828@iit.ac.lk>
2021-04-04 18:16:42 +0530

    remove vendored libraries

--- lambdas/payment/payment_handler.py
+++ lambdas/payment/payment_handler.py
@@ -2,14 +2,16 @@ import json
 import boto3
 import base64
 from botocore.config import Config
-from utils import offline_checker
+import os
+
+offline = os.environ.get("IS_OFFLINE")
 
 def save_payment(event, context):
     statusCode = 200
     responseBody = {'data': ''}
 
     try:
-        if offline_checker.check_offline():
+        if offline == "true":
             dynamodb = boto3.resource('dynamodb', endpoint_url='http://localhost:8000', region_name='us-west-2')
             req = json.loads(event['body'])
         else:
--- vendored-libraries/utils/offline_checker.py
+++
@@ -1,6 +0,0 @@
-import os
-
-def check_offline():
-    env = os.environ.get("IS_OFFLINE")
-    print(env)
-    return env == "true"
