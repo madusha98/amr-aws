90d0d4fbd8650af0ef3b8835230bbe5085f058e6
madushalakruwan <madusha.2018828@iit.ac.lk>
2021-04-07 13:44:39 +0530

    add authorizer to lambda functions

--- lambdas/handler.py
+++ lambdas/handler.py
@@ -1,33 +1,14 @@
-import simplejson as json
-import boto3
-
+import json
 
 def hello(event, context):
-    # dynamodb = boto3.resource('dynamodb')
-    # dynamodb = boto3.resource('dynamodb', endpoint_url='http://localhost:8000', region_name='us-west-2') # local
-    # try:
-    #     table = dynamodb.Table('monthlyReadingTable')
-    #     # item = table.get_item(Key={'accId': 'abc'})
-    #     # print(item)
-    #     response = table.scan()
-    #     items = response['Items']
-    #     print(items)
-    #     body = {
-    #     "message": "Go Serverless v1.0! Your function executed successfully!",
-    #     "data": items
-    #     }
-
-    # except Exception as e:
-    #     print('Exception: ', e)
 
     body = {
             "message": "Go Serverless v1.0! Your function executed successfully!"
     }
 
-
     response = {
         "statusCode": 200,
-        "body": json.dumps(body, use_decimal=True)
+        "body": json.dumps(body)
     }
 
     return response
