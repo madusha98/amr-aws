3b8d7d3217724c8207d23a0226a71909a291ea07
Ruvindra Senevirathne <ruvindra.2018848@iit.ac.lk>
2021-04-10 23:21:05 +0530

    Get Account Details API added

---
+++ lambdas/account/account_details_handler.py
@@ -0,0 +1,35 @@
+import json
+import base64
+import boto3
+from boto3.dynamodb.conditions import Key, Attr
+import os
+
+offline = os.environ.get("IS_OFFLINE")
+stage = os.environ.get("stage")
+
+def get_account_detaills(event, context):
+    statusCode = 200
+    dynamodb = boto3.resource('dynamodb')
+
+    try:
+        if offline == "true":
+            dynamodb = boto3.resource('dynamodb', endpoint_url='http://localhost:8000', region_name='us-west-2')
+        else:
+            dynamodb = boto3.resource('dynamodb')
+        table = dynamodb.Table('accountTable-' + stage)
+        response = table.scan(
+            FilterExpression=Attr('userId').eq(event["queryStringParameters"]['userId'])
+        )
+        print(response)
+        responseBody=json.dumps(response['Items'])
+    except Exception as e:
+        print('Exception: ', e)
+        statusCode = 500
+        responseBody = {"error": str(e)}
+
+    responseJson = {
+        "statusCode": statusCode,
+        "body": responseBody
+    }
+
+    return responseJson
