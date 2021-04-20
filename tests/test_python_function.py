5eaa7cc96c829ce680bd56dff1660e1b790038c7
madushalakruwan <madusha.2018828@iit.ac.lk>
2021-04-21 00:03:11 +0530

    unit testing for location validation

--- tests/test_python_function.py
+++ tests/test_python_function.py
@@ -1,12 +1,18 @@
 import unittest
 from lambdas.handler import hello
 from tests import *
+from lambdas.meter_reader import validate_location
 
 class TestStringMethods(unittest.TestCase):
 
     def test_hello(self):
         resp = hello(api_gateway_event({}), None)
         self.assertEqual(resp, {'statusCode': 200, 'body': '{"message": "Go Serverless v1.0! Your function executed successfully!"}'})
+
+    def test_validate_location(self):
+        self.assertEqual(validate_location.validate_location('6.682696, 80.325955', '6.682721, 80.325954'), True )
+        self.assertEqual(validate_location.validate_location('6.682696, 80.325955', '6.649145, 80.350719'), False )
+
 
 if __name__ == '__main__':
     unittest.main()
